05369aed7a2ca4108672da5cf248d3dad978a20e
RascalNiki <seichi.rascal97@gmail.com>
2025-06-03 16:49:25 +0900

    Add files via upload

---
+++ SeatShuffler.cs
@@ -0,0 +1,60 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class SeatShuffler : UdonSharpBehaviour
+{
+    public SeatTile[] tiles;
+
+    private Vector3[] _originalPositions;
+
+    void Start()
+    {
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+        _originalPositions = new Vector3[tiles.Length];
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            _originalPositions[i] = tiles[i].transform.position;
+        }
+    }
+
+    public override void Interact()
+    {
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        ShuffleTiles();
+        ResetTiles();
+    }
+
+    private void ResetTiles()
+    {
+        foreach (SeatTile tile in tiles)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, tile.gameObject);
+            tile.HideTile();
+        }
+    }
+
+    private void ShuffleTiles()
+    {
+        for (int i = tiles.Length - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+
+            Vector3 tempPos = _originalPositions[i];
+            _originalPositions[i] = _originalPositions[j];
+            _originalPositions[j] = tempPos;
+        }
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, tiles[i].gameObject);
+            tiles[i].SetPosition(_originalPositions[i]);
+        }
+    }
+}
---
+++ SeatTile.cs
@@ -0,0 +1,64 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class SeatTile : UdonSharpBehaviour
+{
+    public Transform tileTransform;
+    public AudioSource revealSound;
+
+    private Quaternion _frontRotation = Quaternion.Euler(-90f, 180f, 0f);
+    private Quaternion _backRotation = Quaternion.Euler(90f, 0f, 0f);
+
+    [UdonSynced]
+    private bool _isRevealed = true;
+
+    [UdonSynced]
+    private Vector3 _syncedPosition;
+
+    public override void Interact()
+    {
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+        if (!_isRevealed)
+        {
+            RevealTile();
+            revealSound.Play();
+            RequestSerialization();
+        }
+    }
+
+    public void RevealTile()
+    {
+        _isRevealed = true;
+        SetFace(true);
+        RequestSerialization();
+    }
+
+    public void HideTile()
+    {
+        _isRevealed = false;
+        SetFace(false);
+        RequestSerialization();
+    }
+
+    public void SetPosition(Vector3 pos)
+    {
+        _syncedPosition = pos;
+        transform.position = pos;
+        RequestSerialization();
+    }
+
+    private void SetFace(bool showFront)
+    {
+        tileTransform.localRotation = showFront ? _frontRotation : _backRotation;
+    }
+
+    public override void OnDeserialization()
+    {
+        transform.position = _syncedPosition;
+        SetFace(_isRevealed);
+    }
+}
